34f9c129a8bf0adac3aab350c5eb3898c433ee44
Hamza Sheikh <hsheikhm@gmail.com>
2016-02-19 12:07:02 +0000

    added script for submitting new peep form when enter key is pressed

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,14 +1,28 @@
 (function(){
 
-  function characterCount() {
+  function characterCount(e) {
     var textEntered, charDisplay, counter;
+
     textEntered = document.getElementById('peep-text').value;
     charDisplay = document.getElementById('count');
     counter = (140 - (textEntered.length));
     charDisplay.textContent = counter;
+
+    wasEnterKeyPressed(e);
+  }
+
+  function wasEnterKeyPressed(e) {
+    var newPeepForm;
+
+    newPeepForm = document.getElementById('new-peep-form');
+    if(e.keyCode === 13){
+      newPeepForm.submit();
+    }
   }
 
   var elText = document.getElementById('peep-text');
-  elText.addEventListener('keyup', characterCount, false);
+  elText.addEventListener('keyup', function(e) {
+    characterCount(e);
+  }, false);
 
 }());
